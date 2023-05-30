8639ea8253ca7c40a323d320a2e07f640dbe204a
jpalex <aluno80433@ipt.pt>
2023-05-30 16:43:20 +0100

    First commit

---
+++ build/lib/joaoAlexandre.py
@@ -0,0 +1,10 @@
+def Euros2Libras(euro):
+    fatorConver = 1.1513298
+    libra = euro * fatorConver
+    print('%0.2f euro dá %0.2f libra' %(euro,libra))
+
+
+def Kms2Milhas(klms):
+    fatorConver = 0.621371
+    milhas = klms * fatorConver
+    print('%0.2f kilometro em milha dá %0.2f milha' %(klms,milhas))
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='oaoAlexandre',
+      version='1.0',
+      description='joaoAlexandre Module created for LP academic propose with funtions Euro2Libras e Kms2Milhas',
+      author='JP Alexandre - 80433',
+      author_email='aluno80433@ipt.pt',
+      url='https://github.com/jpalex/joaoAlexandre',
+      py_modules=['joaoAlexandre'],
+     )
